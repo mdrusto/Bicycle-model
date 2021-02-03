21240c03ecd78d1a080226525110c9448f1b4728
yafta98 <yafta98@gmail.com>
2021-02-03 17:48:03 -0500

    Analysis changes

--- parameter_analysis.m
+++ parameter_analysis.m
@@ -12,9 +12,10 @@ model_workspace = get_param(model_name, 'ModelWorkspace');
 original_val = getVariable(model_workspace, param);
 
 for i = 1:length(vals)
+    disp(['Starting simulation #' num2str(i) '/' num2str(length(vals))]);
     % Change the parameter of interest
-    %inputs = inputs.setModelParameter([model_name '/' param], vals(i));
-    assignin(model_workspace, param, vals(i));
+    %inputs = inputs.setBlockParameter([model_name '/' param], 'Value', num2str(vals(i));
+    model_workspace.assignin(param, vals(i));
     
     % DELTA = 0, BETA = 0
     % Change the delta and beta inputs in the model
@@ -22,12 +23,14 @@ for i = 1:length(vals)
     inputs = inputs.setBlockParameter(strcat(model_name, '/Chassis slip angle, deg'), 'Value', num2str(0));
     outputs = sim(inputs); % Run the simulation
     original_yaw_moment = outputs.logsout{2}.Values.Data(1); % Record the outputs
+    %disp(['Original yaw moment: ' num2str(original_yaw_moment) 'Nm'])
     
     % DELTA = 0.01, BETA = 0
     % Change the delta input in the model
     inputs = inputs.setBlockParameter(strcat(model_name, '/Steering angle, deg'), 'Value', num2str(0.01));
     outputs = sim(inputs); % Run the simulation
     control_yaw_moment = outputs.logsout{2}.Values.Data(1); % Record the outputs
+    %disp(['Control yaw moment: ' num2str(control_yaw_moment) 'Nm'])
     
     % DELTA = 0, BETA = 0.01
     % Reset the delta input and change the beta input in the model
@@ -35,6 +38,7 @@ for i = 1:length(vals)
     inputs = inputs.setBlockParameter(strcat(model_name, '/Chassis slip angle, deg'), 'Value', num2str(0.01));
     outputs = sim(inputs); % Run the simulation
     stability_yaw_moment = outputs.logsout{2}.Values.Data(1); % Record the outputs
+    %disp(['Stability yaw moment: ' num2str(stability_yaw_moment) 'Nm'])
 
     control(i) = (control_yaw_moment - original_yaw_moment) / 0.01;
     stability(i) = (stability_yaw_moment - original_yaw_moment) / 0.01;
@@ -51,7 +55,7 @@ plot(vals, stability)
 ylabel('Stability (Nm/deg)')
 title(['Control and stability with changes in ' titlestring])
 xlabel(x_label)
-ylim([-inf 0])
+ylim([0 inf])
 
 % Reset the model workspace variable
 assignin(model_workspace, param, original_val);
